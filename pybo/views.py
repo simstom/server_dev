07f52988a8cb506e2e4ef1532da91e68a22e3728
simstom <yckimyck62@daum.net>
2021-11-16 21:28:14 +0900

    css added

--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
 # from django.http import HttpResponse
@@ -7,8 +7,9 @@ from django.shortcuts import render
 # def index(request):
 #     return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
 
-from django.shortcuts import render
-from .models import Question
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question, Answer
 
 
 def index(request):
@@ -24,6 +25,17 @@ def detail(request, question_id):
     """
     pybo 내용 출력
     """
-    question = Question.objects.get(id=question_id)
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    # question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    answer.save()
+    return redirect('pybo:detail', question_id=question.id)
